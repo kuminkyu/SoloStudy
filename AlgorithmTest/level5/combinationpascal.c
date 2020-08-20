9d9df7dcede086db1be7b7c36c2c3ea6582e4f7c
kuminkyu <kuminkyu93@naver.com>
2020-08-20 13:38:14 +0900

    Create combinationpascal.c

---
+++ AlgorithmTest/level5/combinationpascal.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int pascal(int a,int b){
+  
+  int t;
+  if(b==0) return 1;
+  if(a==b) return 1;
+  t = pascal(a-1,b-1)+pascal(a-1,b);
+  
+  return t;
+}
+int main(){
+  
+  int n,m;
+  
+  scanf("%d %d",&n,&m);
+  
+  printf("%d",pascal(n,m));
+  return 0;
+}
